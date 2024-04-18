2b9136fbe7e4d874c52b8bc44054ed22aeff7bff
Ilya <ilya.rodin.04@bk.ru>
2024-04-17 18:33:58 -0700

    Initial commit

---
+++ game.py
@@ -0,0 +1,19 @@
+"""Игра угадай число"""
+import numpy as np
+
+number = np.random.randint(1, 101) # загадываем число
+
+#число попыток
+count = 0
+
+while True:
+    count+=1
+    predict_number=int(input("Угадай число от 1 до 100: "))
+    
+    if predict_number > number:
+        print("Число должно быть меньше!")
+    elif predict_number < number:
+        print("Число должно быть больше!")
+    else:
+        print(f'Вы угадали число {number}, за {count} попыток')
+        break #остановка бесконечного цикла
